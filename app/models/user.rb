6dd321169ff2b7a98ae4eec843b16a64e8a85b5e
virajvchavan <viraj@joshsoftware.com>
2018-09-08 11:34:40 +0530

    loaded some users from a json

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+
+	def self.import_users_from_json
+		users = JSON.parse(File.read(Rails.root.join('config/users.json')))['data']
+		users.each do |user|
+			User.create(:name => user['name'], :position => user['position'], :salary => user['salary'].gsub(',', '')[1..100], :office => user['office'], :start_date => Date.parse(user['start_date']['display']), :extn => user['extn'])
+		end
+	end
 end
